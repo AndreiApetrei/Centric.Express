7f5ea02ce1d3488ded044973f293914fc02b39d9
Daniel Gavril <gavrildaniel2004@yahoo.com>
2018-02-10 10:41:08 +0200

    Added items controller.

---
+++ backend/CentricExpress/CentricExpress/Controllers/ItemsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentricExpress.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/items")]
+    public class ItemsController : Controller
+    {
+        // GET: api/Item
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Item/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Item
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Item/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
