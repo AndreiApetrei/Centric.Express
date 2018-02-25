35f2b1e87562e7685a35e271f69bae27473983b9
Patrascu <george.patrascu@centric.eu>
2018-02-25 12:52:35 +0200

    Add OrderContoller

---
+++ backend/CentricExpress/CentricExpress/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CentricExpress.WebApi.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return
+            View();
+        }
+    }
+}
