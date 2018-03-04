7c1e6415e6873da4457ebecfca55782c80bbc2a3
Nechita Francesca <nechita.francesca@gmail.com>
2018-03-04 15:04:25 +0200

    Added tests for item dto validator.

---
+++ backend/CentricExpress/CentricExpress.Business.Tests/ValidatorTests/ItemDtoValidatorTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+using CentricExpress.Business.DTOs;
+using CentricExpress.Business.Validators;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CentricExpress.Business.Tests.ValidatorTests
+{
+    [TestClass]
+    public class ItemDtoValidatorTests
+    {
+        private static ItemDtoValidator SystemUnderTest;
+
+        [ClassInitialize]
+        public static void ClassSetup(TestContext testContext)
+        {
+            SystemUnderTest = new ItemDtoValidator();
+        }
+
+        [TestMethod]
+        public void Given_OtherPropsAreValid_When_DescriptionHas15Chars_Then_ShouldReturnValid()
+        {
+            //arrange
+            var itemDto = new ItemDto
+                          {
+                              Description = "somedescription",
+                              Price = 10
+                          };
+
+            //act
+            var result = SystemUnderTest.Validate(itemDto);
+
+            //assert
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void Given_OtherPropsAreValid_When_DescriptionIs3008Chars_Then_ShouldReturnLengthError()
+        {
+            //arrange
+            var expectedMessage = "Description's length must be between 10 and 3000";
+
+            var _10charsDescription = "somedescription ";
+            var longDescription = new StringBuilder(_10charsDescription);
+            while (longDescription.Length <= 3000)
+            {
+                longDescription.Append(_10charsDescription);
+            }
+
+            var itemDto = new ItemDto
+                          {
+                              Description = longDescription.ToString(),
+                              Price = 10
+                          };
+
+            //act
+            var result = SystemUnderTest.Validate(itemDto);
+
+            //assert
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Any(vf => vf.ErrorMessage.Equals(expectedMessage, StringComparison.InvariantCultureIgnoreCase)));
+        }
+
+        [TestMethod]
+        public void Given_OtherPropsAreValid_When_DescriptionIsLessThanTenChars_Then_ShouldReturnLengthError()
+        {
+            //arrange
+            var expectedMessage = "Description's length must be between 10 and 3000";
+            var itemDto = new ItemDto
+                          {
+                              Description = "somedesc",
+                              Price = 10
+                          };
+
+            //act
+            var result = SystemUnderTest.Validate(itemDto);
+
+            //assert
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Any(vf => vf.ErrorMessage.Equals(expectedMessage, StringComparison.InvariantCultureIgnoreCase)));
+        }
+
+        [TestMethod]
+        public void Given_OtherPropsAreValid_When_PriceIsMinusOne_Then_ShouldReturnGreaterThanError()
+        {
+            //arrange
+            var itemDto = new ItemDto
+                          {
+                              Description = "somedescription",
+                              Price = -1
+                          };
+
+            //act
+            var result = SystemUnderTest.Validate(itemDto);
+
+            //assert
+            Assert.IsFalse(result.IsValid);
+            Assert.IsTrue(result.Errors.Any(vf => vf.PropertyName == nameof(ItemDto.Price)));
+        }
+
+        [TestMethod]
+        public void Given_OtherPropsAreValid_When_PriceIsTen_Then_ShouldReturnValid()
+        {
+            //arrange
+            var itemDto = new ItemDto
+                          {
+                              Description = "somedescription",
+                              Price = 10
+                          };
+
+            //act
+            var result = SystemUnderTest.Validate(itemDto);
+
+            //assert
+            Assert.IsTrue(result.IsValid);
+        }
+    }
+}
